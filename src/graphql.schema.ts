5351c84c8bbd52f76de42553af25ddbd05cbc747
Roland <roland.paire@gmx.fr>
2023-04-11 15:01:27 +0200

    chore: format

--- src/graphql.schema.ts
+++ src/graphql.schema.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -9,55 +8,63 @@
 /* eslint-disable */
 
 export class CreateCatInput {
-    name?: Nullable<string>;
-    age?: Nullable<number>;
+  name?: Nullable<string>;
+  age?: Nullable<number>;
 }
 
 export class CreateQuoteInput {
-    name?: Nullable<string>;
-    age?: Nullable<number>;
+  name?: Nullable<string>;
+  age?: Nullable<number>;
 }
 
 export abstract class IQuery {
-    abstract cats(): Nullable<Nullable<Cat>[]> | Promise<Nullable<Nullable<Cat>[]>>;
+  abstract cats():
+    | Nullable<Nullable<Cat>[]>
+    | Promise<Nullable<Nullable<Cat>[]>>;
 
-    abstract cat(id: string): Nullable<Cat> | Promise<Nullable<Cat>>;
+  abstract cat(id: string): Nullable<Cat> | Promise<Nullable<Cat>>;
 
-    abstract quotes(): Nullable<Nullable<Quote>[]> | Promise<Nullable<Nullable<Quote>[]>>;
+  abstract quotes():
+    | Nullable<Nullable<Quote>[]>
+    | Promise<Nullable<Nullable<Quote>[]>>;
 
-    abstract quote(id: string): Nullable<Quote> | Promise<Nullable<Quote>>;
+  abstract quote(id: string): Nullable<Quote> | Promise<Nullable<Quote>>;
 }
 
 export abstract class IMutation {
-    abstract createCat(createCatInput?: Nullable<CreateCatInput>): Nullable<Cat> | Promise<Nullable<Cat>>;
+  abstract createCat(
+    createCatInput?: Nullable<CreateCatInput>,
+  ): Nullable<Cat> | Promise<Nullable<Cat>>;
 
-    abstract createQuote(createQuoteInput?: Nullable<CreateQuoteInput>): Nullable<Quote> | Promise<Nullable<Quote>>;
+  abstract createQuote(
+    createQuoteInput?: Nullable<CreateQuoteInput>,
+  ): Nullable<Quote> | Promise<Nullable<Quote>>;
 }
 
 export abstract class ISubscription {
-    abstract catCreated(): Nullable<Cat> | Promise<Nullable<Cat>>;
+  abstract catCreated(): Nullable<Cat> | Promise<Nullable<Cat>>;
 
-    abstract quoteCreated(): Nullable<Quote> | Promise<Nullable<Quote>>;
+  abstract quoteCreated(): Nullable<Quote> | Promise<Nullable<Quote>>;
 }
 
 export class Owner {
-    id: number;
-    name: string;
-    age?: Nullable<number>;
-    cats?: Nullable<Cat[]>;
+  id: number;
+  name: string;
+  age?: Nullable<number>;
+  cats?: Nullable<Cat[]>;
 }
 
 export class Cat {
-    id?: Nullable<number>;
-    name?: Nullable<string>;
-    age?: Nullable<number>;
-    owner?: Nullable<Owner>;
+  id?: Nullable<number>;
+  name?: Nullable<string>;
+  age?: Nullable<number>;
+  owner?: Nullable<Owner>;
 }
 
 export class Quote {
-    id?: Nullable<number>;
-    name?: Nullable<string>;
-    age?: Nullable<number>;
+  id?: Nullable<number>;
+  name?: Nullable<string>;
+  age?: Nullable<number>;
 }
 
 type Nullable<T> = T | null;
