1a37c4672365fef0d834f3a12306467446e65300
Roland <roland.paire@gmx.fr>
2023-10-03 11:26:03 +0200

    ci: test secret env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,10 +8,7 @@ import { DatabaseService } from './database.service';
   imports: [
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        uri:
-          configService.get<string>('NODE_ENV') === 'test'
-            ? configService.get<string>('MONGO_TEST_CONNECTION_URI')
-            : configService.get<string>('MONGO_CONNECTION_URI'),
+        uri: configService.get<string>('MONGO_CONNECTION_URI'),
       }),
       inject: [ConfigService],
     }),
